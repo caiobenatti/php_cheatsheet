c6ba30f2ead1fb0c9b1cac44ccaef39723410e27
caio benatti <caio.benatti@gmail.com>
2020-10-20 13:14:15 +0100

    function declarations

--- tuts/index.php
+++ tuts/index.php
@@ -1,20 +1,26 @@
 <?php 
 
 //echo 'hello world';
-$firstName = 'Caio';
-$lastName = 'Benatti';
+// $firstName = 'Caio';
+// $lastName = 'Benatti';
 
-$products = [ 
-    ['name' => 'shiny star', 'price' => 20],
-    ['name' => 'green shell', 'price' => 10],
-    ['name' => 'red shell', 'price' => 15],
-    ['name' => 'gold coin', 'price' => 5],
-    ['name' => 'lightining bolt', 'price' => 40],
-    ['name' => 'banana skin', 'price' => 2],
-]
+// $products = [ 
+//     ['name' => 'shiny star', 'price' => 20],
+//     ['name' => 'green shell', 'price' => 10],
+//     ['name' => 'red shell', 'price' => 15],
+//     ['name' => 'gold coin', 'price' => 5],
+//     ['name' => 'lightining bolt', 'price' => 40],
+//     ['name' => 'banana skin', 'price' => 2],
+// ]
+
+function sayHello($name){
+    echo "Good morning $name";
+}
+
+sayHello('Teste');
 
 ?>
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -33,5 +39,6 @@ $products = [
     <?php } ?>
     </ul>
     </div>
+
 </body>
-</html>
+</html> -->
