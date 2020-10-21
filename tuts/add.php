1a7fc13e20439cbaaee9979aad6bc066f0329e6b
caio benatti <caio.benatti@gmail.com>
2020-10-21 14:58:29 +0100

    form validation in php

--- tuts/add.php
+++ tuts/add.php
@@ -1,7 +1,54 @@
 <?php
 
+// if (isset($_GET['submit'])) {
+//     echo $_GET['email'];
+//     echo $_GET['title'];
+//     echo $_GET['ingredients'];
+// }
+
+
+// check if the form is submitted
+if (isset($_POST['submit'])) {
+
+    //check email
+    if (empty(trim($_POST['email']))) {
+        echo 'An email is required <br />';
+    } else {
+        //echo htmlspecialchars($_POST['email']); //htmlspecialchars prevent XSS basic attack
+        $email = $_POST['email'];
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            echo 'Email must be valid email address';
+        }
+    }
+    // check title
+    if (empty(trim($_POST['title']))) { //use the trim to avoid sending blank pages
+        echo 'A Name is required <br />';
+    } else {
+        // echo htmlspecialchars($_POST['title']);
+        $title = $_POST['title'];
+        if (!preg_match('/^[a-zA-Z\s]+$/', $title)) {
+            echo 'Title must be letters and spaces only';
+        }
+    }
+    //check ingredients
+    if (empty(trim($_POST['ingredients']))) {
+        echo 'The list of ingredients is required <br />';
+    } else {
+        // echo htmlspecialchars($_POST['ingredients']);
+        $ingredients = $_POST['ingredients'];
+        if (!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)) {
+            echo 'Ingredients must be a comma separated list <br />';
+        }
+    }
+} // end of post check
+
+
+// form validation
+
+
 
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -11,7 +58,7 @@
 <section class="container grey-text">
 
     <h4 class="center">Add a Pizza</h4>
-    <form class="white" action="" method="">
+    <form class="white" action="add.php" method="POST">
         <label>Your email:</label>
         <input type="email" name="email">
         <label>Pizza Title:</label>
@@ -19,7 +66,7 @@
         <label>Ingredients (comma separated):</label>
         <input type="text" name="ingredients">
         <div class="center">
-            <input type="submit" name="submit" class="btn brand z-depth-0">
+            <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
         </div>
     </form>
 
